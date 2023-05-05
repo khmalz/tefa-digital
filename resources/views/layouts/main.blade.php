8d4779359316133548ecba2f7ad81d38c5f549b8
Advenian <mpradipta.arya02@gmail.com>
2023-05-05 14:38:12 +0700

    add: added background image

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -77,17 +77,18 @@
             class="bi bi-arrow-up-short"></i>
     </a>
 
+
+
     <!-- Vendor JS Files -->
-    <script src="{{ asset('assets/vendor/aos/aos.js')}}"></script>
-    <script src="{{ asset('assets/vendor/bootstrap/js.bootstrap.bundle.min.js')}}"></script>
-    <script src="{{asset ('assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
-    <script src="{{ asset('assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
-    <script src="{{ asset('assets/vendor/swiper-bundle.min.js')}}"></script>
-    <script src="{{ asset('assets/vendor/php-email-form.js/validate.js') }}"></script>
+    <script src="{{ asset('assets/vendor/aos/aos.js') }}"></script>
+    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/swiper/swiper-bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/php-email-form/validate.js') }}"></script>
 
     <!-- Template Main JS File -->
-    <script src="{{ asset('assets/js/main.js')}} "></script>
-
+    <script src="{{ asset('assets/js/main.js') }}"></script>
 </body>
 
 </html>
