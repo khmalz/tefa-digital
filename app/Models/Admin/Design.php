fb4c10735e2915547f28303b91ff167d6497f7d2
khmalz <akmalrma1710@gmail.com>
2023-05-10 21:46:37 +0700

    fix: menghapus method category

--- app/Models/Admin/Design.php
+++ app/Models/Admin/Design.php
@@ -5,12 +5,10 @@ namespace App\Models\Admin;
 use App\Helpers\MixCaseULID;
 use App\Models\Admin\DesignPlan;
 use App\Models\Admin\DesignImage;
-use App\Models\Admin\DesignCategory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 
 class Design extends Model
 {
@@ -38,8 +36,18 @@ class Design extends Model
         'description'
     ];
 
-    protected $with = ['plan', 'category:design_categories.title'];
-    protected $appends = ['price'];
+    protected $with = ['plan.category'];
+    protected $appends = ['price', 'order'];
+
+    public function getPriceAttribute()
+    {
+        return $this->plan->price;
+    }
+
+    public function getOrderAttribute()
+    {
+        return $this->plan->category->title;
+    }
 
     public function plan(): BelongsTo
     {
@@ -49,26 +57,5 @@ class Design extends Model
     public function images(): HasMany
     {
         return $this->hasMany(DesignImage::class, 'design_id');
-    }
-
-    public function category(): HasOneThrough
-    {
-        return $this->hasOneThrough(
-            DesignCategory::class, // Model target
-            DesignPlan::class,
-            // Model perantara
-            'design_category_id',
-            // foreign key pada model DesignPlan
-            'id',
-            // foreign key pada model DesignCategory
-            'id',
-            // local key pada model Design
-            'design_category_id' // local key pada model DesignPlan
-        );
-    }
-
-    public function getPriceAttribute()
-    {
-        return $this->plan->price;
     }
 }
