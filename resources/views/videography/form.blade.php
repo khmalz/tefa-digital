bd22436f0ab988a9e34def751b2092169d218455
nesyafakhira <nesyafkhrarianti75@gmail.com>
2023-06-12 14:42:47 +0700

    refactor videography form

--- resources/views/videography/form.blade.php
+++ resources/views/videography/form.blade.php
@@ -20,26 +20,46 @@
             text-decoration: none;
             justify-content: center;
         }
+        .btn-form {
+            --bs-btn-padding: .100rem;
+            --bs-btn-color: var(--bs-white);
+            --bs-btn-bg: #ef6603;
+            --bs-btn-border-color: #ef6603;
+            --bs-btn-hover-color: #ef6603;
+            --bs-btn-hover-border-color: #ef6603;
+            --bs-btn-focus-shadow-rgb: #ef6603;
+            --bs-btn-active-color: var(--bs-btn-hover-color);
+        }
+        .bg-form {
+        height: 100vh;
+        background: linear-gradient(#ffff 40%, #ef6603 40%);
+        }
+        .card-title {
+            justify-content: center;
+            font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif'
+        }
     </style>
 </head>
 
-<body style="background-color: #e7f3f7">
-    <header class="w-100">
-        <div class="d-flex align-items-center justify-content-center" style="background-color: rgba(42, 44, 57, 0.9)">
-            <div class="logo">
-                <h1>Tefa Digital</h1>
+<body>
+    <div class="bg-form">
+        <header class="w-100">
+            <div class="d-flex align-items-center justify-content-center" style="background-color: rgba(42, 44, 57, 0.9)">
+                <div class="logo">
+                    <h1>Tefa Digital</h1>
+                </div>
             </div>
-        </div>
-    </header>
+        </header>
+    </div>
 
     <div class="d-flex justify-content-center position-absolute top-50 start-50 translate-middle container">
         <div class="section-wrapper w-100">
             <div class="row justify-content-center">
                 <div class="col-md-8 col-12">
-                    <div class="card justify-content-center mb-3 text-center" style="background-color: #e46409;">
-                        <div class="card-body">
-                            <div class="mb-4">
-                                <h2 class="card-title" style="color: white">Form Pemesanan Videografi</h2>
+                    <div class="card justify-content-center mb-3 text-center" style="background-color: #ffff;">
+                        <div class="card-body justify-content-center">
+                            <div class="mb-5">
+                                <h2 class="card-title text-center" style="color: black">Form Pemesanan Videografi</h2>
                             </div>
                             <div class="form-floating mb-3">
                                 <input type="text" class="form-control" id="floatingInput" placeholder="">
@@ -53,12 +73,12 @@
                                 <input type="email" class="form-control" id="floatingInput" placeholder="">
                                 <label for="floatingInput">Email</label>
                             </div>
-                            <div class="form-floating mb-3">
+                            <div class="form-floating mb-4">
                                 <input type="text" class="form-control" id="floatingInput" placeholder="">
                                 <label for="floatingInput">Deskripsi</label>
                             </div>
-                            <div>
-                                <button type="button" class="btn btn-outline-dark">Pesan</button>
+                            <div class="mb-1">
+                                <button type="button" class="btn btn-form rounded-5">Pesan</button>
                             </div>
                         </div>
                     </div>
