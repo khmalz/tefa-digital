3c86e7d96e0c15f000750ae506652542a29c9409
khmalz <akmalrma1710@gmail.com>
2023-06-16 06:41:24 +0700

    fix: middleware untuk halaman yang terbuka untuk semua

--- routes/web.php
+++ routes/web.php
@@ -14,81 +14,79 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('guest')->group(function () {
+Route::get('/', function () {
+    return view('index');
+});
+
+route::prefix('photography')->group(function () {
     Route::get('/', function () {
-        return view('index');
+        return view('photography.index');
     });
 
-    route::prefix('photography')->group(function () {
-        Route::get('/', function () {
-            return view('photography.index');
-        });
-
-        Route::get('/foto-produk', function () {
-            return view('photography.foto-produk');
-        });
-
-        Route::get('/foto-acara', function () {
-            return view('photography.foto-acara');
-        });
-
-        Route::get('/foto-pernikahan', function () {
-            return view('photography.foto-pernikahan');
-        });
-
-        Route::get('/form', function () {
-            return view('photography.form');
-        });
+    Route::get('/foto-produk', function () {
+        return view('photography.foto-produk');
     });
 
-    route::prefix('videography')->group(function () {
-        Route::get('/', function () {
-            return view('videography.index');
-        });
-
-        Route::get('/vid-syuting', function () {
-            return view('videography.vid-syuting');
-        });
-
-        Route::get('/vid-dokumentasi', function () {
-            return view('videography.vid-dokumentasi');
-        });
-
-        Route::get('/form', function () {
-            return view('videography.form');
-        });
+    Route::get('/foto-acara', function () {
+        return view('photography.foto-acara');
     });
 
-    route::prefix('design')->group(function () {
-        Route::get('/', function () {
-            return view('design.index');
-        });
-
-        Route::get('/design-logo', function () {
-            return view('design.design-logo');
-        });
-
-        Route::get('/design-promosi', function () {
-            return view('design.design-promosi');
-        });
-
-        Route::get('/design-3d', function () {
-            return view('design.design-3d');
-        });
-
-        Route::get('/form', function () {
-            return view('design.form');
-        });
+    Route::get('/foto-pernikahan', function () {
+        return view('photography.foto-pernikahan');
     });
 
-    route::prefix('printing')->group(function () {
-        Route::get('/', function () {
-            return view('printing.index');
-        });
+    Route::get('/form', function () {
+        return view('photography.form');
+    });
+});
 
-        Route::get('/form', function () {
-            return view('printing.form');
-        });
+route::prefix('videography')->group(function () {
+    Route::get('/', function () {
+        return view('videography.index');
+    });
+
+    Route::get('/vid-syuting', function () {
+        return view('videography.vid-syuting');
+    });
+
+    Route::get('/vid-dokumentasi', function () {
+        return view('videography.vid-dokumentasi');
+    });
+
+    Route::get('/form', function () {
+        return view('videography.form');
+    });
+});
+
+route::prefix('design')->group(function () {
+    Route::get('/', function () {
+        return view('design.index');
+    });
+
+    Route::get('/design-logo', function () {
+        return view('design.design-logo');
+    });
+
+    Route::get('/design-promosi', function () {
+        return view('design.design-promosi');
+    });
+
+    Route::get('/design-3d', function () {
+        return view('design.design-3d');
+    });
+
+    Route::get('/form', function () {
+        return view('design.form');
+    });
+});
+
+route::prefix('printing')->group(function () {
+    Route::get('/', function () {
+        return view('printing.index');
+    });
+
+    Route::get('/form', function () {
+        return view('printing.form');
     });
 });
 
