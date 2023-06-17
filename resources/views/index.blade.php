bcfa7d1ad0b7b0fb60fb6838162ed6f00094c1a6
nursyakilla <nazwaachmad1510@gmail.com>
2023-06-17 09:57:05 +0700

    gambar landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -123,26 +123,26 @@
                 <ul class="nav nav-tabs row d-flex">
                     <li class="nav-item col-3" data-aos="zoom-in">
                         <a class="nav-link active show" data-bs-toggle="tab" href="#tab-1">
-                            <i class="ri-gps-line"></i>
-                            <h4 class="d-none d-lg-block">Modi sit est dela pireda nest</h4>
+                            <i class="bi bi-camera"></i>
+                            <h4 class="d-none d-lg-block">Photography</h4>
                         </a>
                     </li>
                     <li class="nav-item col-3" data-aos="zoom-in" data-aos-delay="100">
                         <a class="nav-link" data-bs-toggle="tab" href="#tab-2">
-                            <i class="ri-body-scan-line"></i>
-                            <h4 class="d-none d-lg-block">Unde praesenti mara setra le</h4>
+                            <i class="bi bi-camera-reels"></i>
+                            <h4 class="d-none d-lg-block">Videography</h4>
                         </a>
                     </li>
                     <li class="nav-item col-3" data-aos="zoom-in" data-aos-delay="200">
                         <a class="nav-link" data-bs-toggle="tab" href="#tab-3">
-                            <i class="ri-sun-line"></i>
-                            <h4 class="d-none d-lg-block">Pariatur explica nitro dela</h4>
+                            <i class="bi bi-flower1"></i>
+                            <h4 class="d-none d-lg-block">Digital Design</h4>
                         </a>
                     </li>
                     <li class="nav-item col-3" data-aos="zoom-in" data-aos-delay="300">
                         <a class="nav-link" data-bs-toggle="tab" href="#tab-4">
-                            <i class="ri-store-line"></i>
-                            <h4 class="d-none d-lg-block">Nostrum qui dile node</h4>
+                            <i class="bi bi-printer"></i>
+                            <h4 class="d-none d-lg-block">Printing</h4>
                         </a>
                     </li>
                 </ul>
@@ -151,125 +151,89 @@
                     <div class="tab-pane active show" id="tab-1">
                         <div class="row">
                             <div class="col-lg-6 order-2 order-lg-1 mt-3 mt-lg-0">
-                                <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
+                                <h3>Memburu Moment yang Mengabadikan Keindahan Dalam Setiap Bidikan</h3>
                                 <p class="fst-italic">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore
-                                    magna aliqua.
+                                    Dalam setiap pertemuan atau acara pasti terdapat moment - moment yang hadir tanpa di duga. 
+                                    Untuk mengabadikan moment - moment tersebut dalam sebuah bidikan, anda membutuhkan sebuah jasa fotografi. 
+                                </p>
+                                <p>
+                                    Sebuah jasa fotografi yang memiliki keahlian profesional dalam mengoperasikan kamera, sehingga dapat 
+                                    menghasilkan gambar - gambar yang indah dan memikat hati Anda. Untuk itu, kami hadir dengan jasa - jasa yang kami miliki, 
+                                    salah satunya adalah jasa fotografi. 
+                                </p>
+                                <p>
+                                    Kami memiliki 3 jenis jasa fotografi yang mungkin Anda butuhkan, diantaranya yaitu:
                                 </p>
                                 <ul>
-                                    <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea
-                                        commodo consequat.</li>
-                                    <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in
-                                        voluptate velit.</li>
-                                    <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea
-                                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta
-                                        storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+                                    <li><i class="ri-check-double-line"></i> Jasa Foto Produk</li>
+                                    <li><i class="ri-check-double-line"></i> Jasa Foto Pernikahan</li>
+                                    <li><i class="ri-check-double-line"></i> Jasa Foto Acara</li>
                                 </ul>
                                 <p>
-                                    Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                    reprehenderit in voluptate
-                                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                                    non proident, sunt in
-                                    culpa qui officia deserunt mollit anim id est laborum
+                                    Untuk mengoperasikan kamera pasti semua orang bisa melakukannya. Tetapi untuk menghasilkan jepretan gambar yang 
+                                    indah dan pas dari segala sisi, semua orang belum tentu bisa melakukannya. Jadi jika Anda membutuhkan jasa fotografi kami,
+                                    kami siap melayani Anda dengan sepenuh hati.
                                 </p>
                             </div>
                             <div class="col-lg-6 order-1 order-lg-2 text-center">
-                                <img src="assets/img/features-1.png" alt="" class="img-fluid">
+                                <img src="assets/img/photo.jpg" alt="" class="img-fluid">
                             </div>
                         </div>
                     </div>
                     <div class="tab-pane" id="tab-2">
                         <div class="row">
                             <div class="col-lg-6 order-2 order-lg-1 mt-3 mt-lg-0">
-                                <h3>Neque exercitationem debitis soluta quos debitis quo mollitia officia est</h3>
+                                <h3 style="margin-top: 100px">Mengubah Moment yang Hadir Menjadi Karya Seni Bergerak</h3>
                                 <p>
-                                    Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                    reprehenderit in voluptate
-                                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                                    non proident, sunt in
-                                    culpa qui officia deserunt mollit anim id est laborum
+                                    Untuk mengubah moment yang hadir menjadi sebuah karya seni bergerak, 
+                                    anda membutuhkan sebuah jasa videografi dan kami hadir untuk itu. 
+                                    Kami hadir dengan jasa videografi yang memiliki pemahaman mendalam serta keahlian profesional 
+                                    dalam pembuatan karya seni bergerak yang anda inginkan.
                                 </p>
-                                <p class="fst-italic">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore
-                                    magna aliqua.
+                                <p>
+                                    Kami memiliki 2 jenis jasa videografi yang mungkin anda butuhkan, diantaranya yaitu:
                                 </p>
                                 <ul>
-                                    <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea
-                                        commodo consequat.</li>
-                                    <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in
-                                        voluptate velit.</li>
-                                    <li><i class="ri-check-double-line"></i> Provident mollitia neque rerum asperiores
-                                        dolores quos qui a. Ipsum neque dolor voluptate nisi sed.</li>
-                                    <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea
-                                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta
-                                        storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+                                    <li><i class="ri-check-double-line"></i> Jasa Video Syuting</li>
+                                    <li><i class="ri-check-double-line"></i> Jasa Video Dokumentasi</li>
                                 </ul>
                             </div>
                             <div class="col-lg-6 order-1 order-lg-2 text-center">
-                                <img src="assets/img/features-2.png" alt="" class="img-fluid">
+                                <img src="assets/img/video.jpg" alt="" class="img-fluid">
                             </div>
                         </div>
                     </div>
                     <div class="tab-pane" id="tab-3">
                         <div class="row">
                             <div class="col-lg-6 order-2 order-lg-1 mt-3 mt-lg-0">
-                                <h3>Voluptatibus commodi ut accusamus ea repudiandae ut autem dolor ut assumenda</h3>
+                                <h3 style="margin-top: 150px">Mengubah Konsep - Konsep yang Telah Anda Buat Menjadi Desain yang Memikat</h3>
                                 <p>
-                                    Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                    reprehenderit in voluptate
-                                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                                    non proident, sunt in
-                                    culpa qui officia deserunt mollit anim id est laborum
-                                </p>
-                                <ul>
-                                    <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea
-                                        commodo consequat.</li>
-                                    <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in
-                                        voluptate velit.</li>
-                                    <li><i class="ri-check-double-line"></i> Provident mollitia neque rerum asperiores
-                                        dolores quos qui a. Ipsum neque dolor voluptate nisi sed.</li>
-                                </ul>
-                                <p class="fst-italic">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore
-                                    magna aliqua.
+                                    Dengan pemahaman mendalam tentang estetika dan keahlian teknis kami, 
+                                    kami menciptakan solusi desain yang unik dan menginspirasi. 
+                                    Dari ilustrasi yang menawan hingga desain papan iklan yang menarik, 
+                                    kami membantu merek Anda mengkomunikasikan pesan dengan gaya yang khas. 
+                                    Jadikan kami mitra kreatif Anda dalam merancang identitas visual yang tak terlupakan
                                 </p>
                             </div>
                             <div class="col-lg-6 order-1 order-lg-2 text-center">
-                                <img src="assets/img/features-3.png" alt="" class="img-fluid">
+                                <img src="assets/img/design.jpg" alt="" class="img-fluid">
                             </div>
                         </div>
                     </div>
                     <div class="tab-pane" id="tab-4">
                         <div class="row">
                             <div class="col-lg-6 order-2 order-lg-1 mt-3 mt-lg-0">
-                                <h3>Omnis fugiat ea explicabo sunt dolorum asperiores sequi inventore rerum</h3>
+                                <h3 style="margin-top: 150px">Mengubah Gagasan Anda menjadi Karya Nyata.</h3>
                                 <p>
-                                    Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                                    reprehenderit in voluptate
-                                    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                                    non proident, sunt in
-                                    culpa qui officia deserunt mollit anim id est laborum
+                                    Dengan teknologi printing terkini dan berbagai opsi material, 
+                                    kami menciptakan cetakan yang menonjol dan berkualitas tinggi.
+                                    Dari brosur yang menggugah minat hingga spanduk yang menarik perhatian, 
+                                    kami membantu merek Anda tampil mencolok dan menghadirkan pesan yang kuat kepada audiens. 
+                                    Jadikan kami mitra dalam mencetak keberhasilan Anda
                                 </p>
-                                <p class="fst-italic">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                                    incididunt ut labore et dolore
-                                    magna aliqua.
-                                </p>
-                                <ul>
-                                    <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea
-                                        commodo consequat.</li>
-                                    <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in
-                                        voluptate velit.</li>
-                                    <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea
-                                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta
-                                        storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
-                                </ul>
                             </div>
                             <div class="col-lg-6 order-1 order-lg-2 text-center">
-                                <img src="assets/img/features-4.png" alt="" class="img-fluid">
+                                <img src="assets/img/print.jpg" alt="" class="img-fluid">
                             </div>
                         </div>
                     </div>
