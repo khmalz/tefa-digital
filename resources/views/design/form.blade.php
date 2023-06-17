0d06fa105c480bf8c3f7752674baf36f9a46b483
khmalz <akmalrma1710@gmail.com>
2023-06-17 09:08:14 +0700

    add: menambahkan field dan javascript yang diperlukan untuk form Design

--- resources/views/design/form.blade.php
+++ resources/views/design/form.blade.php
@@ -39,7 +39,7 @@
     </style>
 </head>
 
-<body>
+<body style="overflow-x: hidden">
     <nav class="w-100 position-absolute top-0" style="z-index: 111">
         <div class="d-flex align-items-center justify-content-center" style="background-color: rgba(42, 44, 57, 0.9)">
             <div class="logo">
@@ -57,56 +57,130 @@
         <div class="card-form position-relative m-auto mb-3 overflow-hidden rounded">
             <h4 class="fw-semibold my-3 text-center text-white">Form Pemesanan Design</h4>
             <div class="card-input position-relative mb-4 overflow-hidden rounded bg-white">
-                <div class="p-5">
-                    <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="floatingInput" placeholder="">
-                        <label for="floatingInput">Nama</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="floatingInput" placeholder="">
-                        <label for="floatingInput">Nomor telepon</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="floatingInput" placeholder="">
-                        <label for="floatingInput">Email</label>
-                    </div>
-                    <div class="form-floating mb-4">
-                        <input type="text" class="form-control" id="floatingInput" placeholder="">
-                        <label for="floatingInput">Deskripsi</label>
-                    </div>
-                    <div class="mb-4">
-                        <button class="btn btn-sm btn-general">Want Upload Image?</button>
-                        <button class="btn btn-sm btn-general">More</button>
-                    </div>
-                    <img class="preview-image d-none img-fluid col-md-8 col-lg-4 mb-2 rounded" alt="preview image">
-                    <div class="d-flex">
-                        <div class="me-3 mb-3">
-                            <input class="form-control form-control-sm" type="file" id="image"
-                                onchange="previewImage(this)">
+                <form action="" method="POST">
+                    @csrf
+                    <div class="p-5">
+                        <div class="mb-3">
+                            <label class="form-label" for="nameInput">Nama</label>
+                            <input type="text" class="form-control" name="name_customer" id="nameInput"
+                                placeholder="">
                         </div>
-                        <div>
-                            <button class="btn btn-sm btn-danger">Delete</button>
+                        <div class="mb-3">
+                            <label class="form-label" for="phoneInput">Nomor telepon</label>
+                            <input type="text" class="form-control" name="number_customer" id="phoneInput"
+                                placeholder="">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="emailInput">Email</label>
+                            <input type="email" class="form-control" name="email_customer" id="emailInput"
+                                placeholder="">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="sloganInput">Slogan</label>
+                            <input type="text" class="form-control" name="slogan" id="sloganInput" placeholder="">
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="colorInput">Color</label>
+                            <div class="d-flex gap-1" id="colorInputSection">
+                                <input type="text" class="form-control form-control-sm" name="color"
+                                    id="colorInput" placeholder="">
+                                <button type="button" onclick="changeInputType()" id="changeColorInput"
+                                    class="btn btn-sm btn-general">Ubah ke Input
+                                    Color?</button>
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="descriptionInput" class="form-label">Description</label>
+                            <textarea class="form-control" id="descriptionInput" rows="3"></textarea>
+                        </div>
+                        <div class="mb-4">
+                            <button class="btn btn-sm btn-general" type="button" id="uploadImage"
+                                onclick="addImage(this)" data-input-image-count="0">Want Upload
+                                Image?</button>
+                            <div id="container-image"></div>
                         </div>
                     </div>
-                </div>
-                <div class="mb-3 text-center">
-                    <button type="button" class="btn btn-general rounded-2">Pesan</button>
-                </div>
+                    <div class="mb-3 text-center">
+                        <button type="button" class="btn btn-general rounded-2">Pesan</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-    </div>
-</body>
-<script>
-    function previewImage(input) {
-        const [file] = input.files;
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"
+        integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <script>
+        const changeInputType = () => {
+            const colorInput = `
+                <input type="color" name="color" class="form-control form-control-color" id="colorInput" value="#ef6603" title="Choose your color" />
+                <button type="button" onclick="changeInputType()" id="changeColorInput" class="btn btn-sm btn-general">Ubah ke Input Teks?</button>
+            `;
 
-        if (file) {
-            const previewImage = document.querySelector('.preview-image');
-            previewImage.classList.remove("d-none");
-            previewImage.src = URL.createObjectURL(file);
+            const textInput = `
+                <input type="text" class="form-control form-control-sm" name="color" id="colorInput" placeholder="">
+                <button type="button" onclick="changeInputType()" id="changeColorInput" class="btn btn-sm btn-general">Ubah ke Input Color?</button>
+            `;
+
+            const currentInputType = $('#colorInput').prop('type');
+            $('#colorInput, #changeColorInput').remove();
+
+            $('#colorInputSection').append(currentInputType === 'text' ? colorInput : textInput);
+        };
+
+        let imageIndexReal = 0;
+
+        const addImage = (el) => {
+            const inputImageCount = $(el).data('input-image-count') + 1;
+            imageIndexReal++;
+
+            const imageSection = `
+                <div id="imageSection${imageIndexReal}" class="mt-2">
+                    <img class="preview-image${imageIndexReal} d-none img-fluid col-md-8 col-lg-4 mb-2 rounded" alt="preview image">
+                    <div class="d-flex">
+                        <div class="me-3 mb-3">
+                            <input class="form-control form-control-sm" name="gambar[]" type="file" id="image${imageIndexReal}" onchange="previewImage(this, ${imageIndexReal})">
+                        </div>
+                        <div>
+                            <button class="btn btn-sm btn-danger" type="button" onclick="deleteImage(${imageIndexReal})">Delete</button>
+                        </div>
+                    </div>
+                </div>
+            `;
+
+            $('#container-image').append(imageSection);
+
+            // Mengubah nilai data-input-image-count pada elemen tombol
+            // Mengubah teks tombol menjadi "More"
+            // Menghilangkan tombol "Want Upload Image?" setelah ada 3 input gambar
+            $(el).data('input-image-count', inputImageCount)
+                .attr('data-input-image-count', inputImageCount)
+                .text('More')
+                .toggle(inputImageCount < 3);
         }
-    }
-</script>
+
+        const deleteImage = (imageIndex) => {
+            // Menghapus parent div dengan id yang sesuai
+            $(`#imageSection${imageIndex}`).remove();
+
+            // Mengurangi nilai data-input-image-count
+            let uploadImage = $('#uploadImage');
+            let currentCount = uploadImage.data('input-image-count');
+            uploadImage.data('input-image-count', currentCount - 1)
+                .attr('data-input-image-count', currentCount - 1);
+
+            // Memeriksa jika data-input-image-count kurang dari 3, maka tampilkan tombol, dan ketika input countnya 0 berubah isi teks ke "Want Upload Image?"
+            (currentCount - 1 < 3) && uploadImage.show();
+            (currentCount - 1 == 0) && uploadImage.text('Want Upload Image?');
+        }
+
+        const previewImage = (input, index) => {
+            const [file] = input.files;
+
+            if (file) {
+                $(`.preview-image${index}`).removeClass("d-none").attr('src', URL.createObjectURL(file));
+            }
+        }
+    </script>
+</body>
 
 </html>
