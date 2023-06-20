8e993265d449ad9dc62d72d1fe979d030ab3bbd7
khmalz <akmalrma1710@gmail.com>
2023-06-20 10:12:42 +0700

    refactor: memindahkan ke Service untuk Generate Invoice

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -7,34 +7,11 @@ use Illuminate\Http\Response;
 use App\Models\Admin\Printing;
 use App\Models\Admin\Photography;
 use App\Models\Admin\Videography;
-use LaravelDaily\Invoices\Invoice;
-use Illuminate\Database\Eloquent\Model;
-use LaravelDaily\Invoices\Classes\Party;
+use App\Services\GenerateInvoice;
 use LaravelDaily\Invoices\Classes\InvoiceItem;
 
 class PDFController extends Controller
 {
-    private function makeInvoice(Model $model, string $type, array $customFields, InvoiceItem $item): Invoice
-    {
-        $client = new Party([
-            'name'          => $model->name_customer,
-            'phone'         => $model->number_customer,
-        ]);
-
-        $customer = new Party([
-            'name' => $type,
-            'custom_fields' => $customFields,
-        ]);
-
-        return Invoice::make('Tefa Digital SMKN 46')
-            ->seller($client)
-            ->buyer($customer)
-            ->date($model->created_at)
-            ->dateFormat('d/m/Y h:i:s')
-            ->filename("$customer->name - $model->ulid")
-            ->addItem($item);
-    }
-
     public function createInvoiceDesign(Design $design): Response
     {
         $customFields = array_filter(
@@ -49,7 +26,12 @@ class PDFController extends Controller
 
         $item = (new InvoiceItem())->title($design->order)->pricePerUnit($design->price);
 
-        $invoice = $this->makeInvoice($design, 'Design', $customFields, $item);
+        $generateInvoice = new GenerateInvoice(
+            model: $design,
+            item: $item
+        );
+
+        $invoice = $generateInvoice->execute('Design', $customFields);
 
         // And return invoice itself to browser or have a different view
         return $invoice->stream();
@@ -65,7 +47,12 @@ class PDFController extends Controller
 
         $item = (new InvoiceItem())->title($photography->order)->pricePerUnit($photography->price);
 
-        $invoice = $this->makeInvoice($photography, 'Photography', $customFields, $item);
+        $generateInvoice = new GenerateInvoice(
+            model: $photography,
+            item: $item
+        );
+
+        $invoice = $generateInvoice->execute('Photography', $customFields);
 
         // And return invoice itself to browser or have a different view
         return $invoice->stream();
@@ -81,9 +68,14 @@ class PDFController extends Controller
 
         $item = (new InvoiceItem())->title($videography->order)->pricePerUnit($videography->price);
 
-        $invoice = $this->makeInvoice($videography, 'Videography', $customFields, $item);
+        $generateInvoice = new GenerateInvoice(
+            model: $videography,
+            item: $item
+        );
 
-        // And return invoice itself to browser or have a different view
+        $invoice = $generateInvoice->execute('Videography', $customFields);
+
+        // Return the invoice as a response
         return $invoice->stream();
     }
 
@@ -97,7 +89,12 @@ class PDFController extends Controller
 
         $item = (new InvoiceItem())->title("Printing")->pricePerUnit(0);
 
-        $invoice = $this->makeInvoice($printing, 'Printing', $customFields, $item);
+        $generateInvoice = new GenerateInvoice(
+            model: $printing,
+            item: $item
+        );
+
+        $invoice = $generateInvoice->execute('Printing', $customFields);
 
         // And return invoice itself to browser or have a different view
         return $invoice->stream();
