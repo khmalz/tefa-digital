fb53fe90e41d3da127a8465836b6f3af21684a2c
khmalz <akmalrma1710@gmail.com>
2023-11-04 12:46:45 +0700

    Add: Register for client

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
     <meta name="description" content="Tefa Digital - Dashboard to manage service orders" />
     <meta name="keyword" content="Tefa Digital, Service, Design, Printing, Videoghraphy, Fotoghraphy" />
-    <title>Login | {{ config('app.name') }}</title>
+    <title>Register | {{ config('app.name') }}</title>
     <!-- Main styles for this application-->
     <link href="{{ asset('assets/admin/css/style.min.css') }}" rel="stylesheet" />
 </head>
@@ -20,11 +20,20 @@
                     <div class="card mx-4 mb-4">
                         <div class="card-body p-4">
                             <div>
-                                <h1>Login</h1>
-                                <p class="text-medium-emphasis">Sign In to your account</p>
+                                <h1>Register</h1>
+                                <p class="text-medium-emphasis">Create your account</p>
                             </div>
-                            <form action="{{ route('login.store') }}" method="post">
+                            <form action="{{ route('register.store') }}" method="post">
                                 @csrf
+                                <div class="input-group mb-3">
+                                    <span class="input-group-text">
+                                        <svg class="icon">
+                                            <use
+                                                xlink:href="{{ asset('assets/admin/vendors/@coreui/icons/svg/free.svg#cil-user') }}">
+                                            </use>
+                                        </svg></span>
+                                    <input class="form-control" name="name" type="text" placeholder="Name" />
+                                </div>
                                 <div class="input-group mb-3">
                                     <span class="input-group-text">
                                         <svg class="icon">
@@ -32,7 +41,7 @@
                                                 xlink:href="{{ asset('assets/admin/vendors/@coreui/icons/svg/free.svg#cil-envelope-open') }}">
                                             </use>
                                         </svg></span>
-                                    <input class="form-control" name="email" type="text" placeholder="Email" />
+                                    <input class="form-control" name="email" type="email" placeholder="Email" />
                                 </div>
                                 <div class="input-group mb-3">
                                     <span class="input-group-text">
@@ -44,8 +53,11 @@
                                     <input class="form-control" name="password" type="password"
                                         placeholder="Password" />
                                 </div>
-                                <button class="btn btn-block btn-info text-white" type="submit">
-                                    Login
+                                <div class="mb-3">
+                                    <p>Already have an account? <a href="{{ route('login.index') }}">Login</a></p>
+                                </div>
+                                <button class="btn btn-block btn-success text-white" type="submit">
+                                    Register
                                 </button>
                             </form>
                         </div>
