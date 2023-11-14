669df5bd1d82e01c0dd457501bc7be53bd738462
khmalz <akmalrma1710@gmail.com>
2023-11-14 10:20:26 +0700

    fix: column footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,105 +1,92 @@
     <!-- ======= Footer ======= -->
     <footer id="footer">
         <div class="container">
-            <div class="row" style="text-align: left" style="text-align: left">
-                <div class="col-4">
+            <div class="row" style="row-gap: 15px">
+                <div class="col-md-4">
                     <h3>Tefa Digital</h3>
+
+                    <span><i>Mendukung kreatifitas siswa berjiwa wirausaha.</i></span>
+                    <div class="mt-3">
+                        <div class="social-links d-flex flex-column" style="row-gap: 5px">
+                            <div class="d-flex align-items-center">
+                                <a target="_blank" href="{{ url('https://www.instagram.com/smknegeri46jakarta') }}"
+                                    class="instagram mt-1">
+                                    <i class="bx bxl-instagram"></i>
+                                </a>
+                                <span>Instagram</span><br>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <a target="_blank"
+                                    href="{{ url('https://smksedkijakarta.wordpress.com/kota/jakarta-timur/smk-negeri-46') }}"
+                                    class="wordpress mt-1">
+                                    <i class="bx bxl-wordpress"></i>
+                                </a>
+                                <span>Wordpress</span><br>
+                            </div>
+                            <div class="d-flex align-items-center">
+                                <a target="_blank" href="{{ url('https://smkn46jaktim.sch.id') }}" class=website mt-1">
+                                    <i class="bx bx-globe"></i>
+                                </a>
+                                <span>Website</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-4">
-                    <h4>Categories</h4>
+                <div class="col-md-4">
+                    <h4 class="position-relative fs-3 p-0" style="margin: 0 0 20px 0;">Categories</h4>
+
+                    <div class="footer-links d-flex flex-column w-full gap-3">
+                        <div>
+                            <a href="">Design</a>
+                        </div>
+                        <div>
+                            <a href="">Photography</a>
+                        </div>
+                        <div>
+                            <a href="">Videography</a>
+                        </div>
+                        <div>
+                            <a href="">Printing</a>
+                        </div>
+                    </div>
                 </div>
-                <div class="col-4">
-                    <h4>Sub-Categories</h4>
+                <div class="col-md-4">
+                    <h4 class="position-relative fs-3 p-0" style="margin: 0 0 20px 0;">Sub-Categories</h4>
+
+                    <div class="footer-links">
+                        <div class="row">
+                            <div class="col-md-6 mb-4">
+                                <span>Design</span><br>
+                                <hr style="width: 25%">
+                                <a href="">3D</a><br>
+                                <a href="">Logo</a><br>
+                                <a href="">Promotion</a>
+                            </div>
+                            <div class="col-md-6 mb-4">
+                                <span>Photography</span><br>
+                                <hr style="width: 25%">
+                                <a href="">Event</a><br>
+                                <a href="">Wedding</a><br>
+                                <a href="">Product</a>
+                            </div>
+                            <div class="col-md-6 mb-4">
+                                <span>Videography</span><br>
+                                <hr style="width: 25%">
+                                <a href="">Documentation</a><br>
+                                <a href="">Shooting</a><br>
+                                <a href="">Commercial</a>
+                            </div>
+                            <div class="col-md-6 mb-4">
+                                <span>Printing</span><br>
+                                <hr style="width: 25%">
+                                <a href="">3D</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-md-4 ">
-                    <div>
-                        <div class="" style="text-align: left">
 
-                            <span><i>Mendukung kreatifitas siswa berjiwa wirausaha.</i></span>
-
-                            <div style="text-align: left" class="mt-3">
-                                <div class="social-links">
-                                    <a target="_blank" href="{{ url('https://www.instagram.com/smknegeri46jakarta') }}"
-                                        class="instagram mt-1"><i
-                                            class="bx bxl-instagram"></i></a><span>Instagram</span><br>
-                                    <a target="_blank"
-                                        href="{{ url('https://smksedkijakarta.wordpress.com/kota/jakarta-timur/smk-negeri-46') }}"
-                                        class="wordpress mt-1"><i
-                                            class="bx bxl-wordpress"></i></a><span>Wordpress</span><br>
-                                    <a target="_blank" href="{{ url('https://smkn46jaktim.sch.id') }}"
-                                        class="website mt-1"><i class="bx bx-globe"></i></a><span>Website</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 mb-3">
-                    <div>
-                        <div style="text-align: left">
-
-                            <div class="footer-links ">
-                                <a href="">Design</a><br>
-                                <a href="">Photography</a><br>
-                                <a href="">Videography</a><br>
-                                <a href="">Printing</a><br>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 ">
-                    <div>
-
-                        <div style="text-align: left">
-                            <div class="footer-links">
-                                <div class="row ">
-                                    <div class="col-md-4  mb-4">
-                                        <span>Design</span><br>
-                                        <hr style="width: 33%">
-
-                                        <a href="">3D</a><br>
-                                        <a href="">Logo</a><br>
-                                        <a href="">Promotion</a>
-
-                                    </div>
-                                    <div class="col-md-4 mb-4">
-                                        <span>Photography</span><br>
-                                        <hr style="width: 33%">
-
-                                        <a href="">Event</a><br>
-                                        <a href="">Wedding</a><br>
-                                        <a href="">Product</a>
-
-
-                                    </div>
-                                    <div class="col-md-4 mb-4">
-                                        <span>Videography</span><br>
-                                        <hr style="width: 33%">
-
-                                        <a href="">Documentation</a><br>
-                                        <a href="">Shooting</a><br>
-                                        <a href="">Commercial</a>
-
-
-                                    </div>
-                                    <div class="col-md-4 mb-4">
-                                        <span>Printing</span><br>
-                                        <hr style="width: 33%">
-                                        <a href="">3D</a>
-
-                                    </div>
-
-
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            <div class="row">
+            <div class="row text-center">
                 <div class="copyright">
                     &copy; Copyright <strong><span>SMK Negeri 46</span></strong>. All Rights Reserved
                 </div>
