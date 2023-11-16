7f62e9c22d60b05c8c3dbf2f1a98d8c8936c5385
khmalz <akmalrma1710@gmail.com>
2023-11-16 21:08:48 +0700

    refactor: create carousel layouts for category

--- resources/views/videography/index.blade.php
+++ resources/views/videography/index.blade.php
@@ -3,42 +3,12 @@
 @include('layouts.category-header')
 
 @section('hero')
-    <!-- ======= Hero Section ======= -->
-    <section id="hero" class="d-flex flex-column justify-content-end align-items-center">
-        <div id="heroCarousel" data-bs-interval="5000" class="carousel carousel-fade" data-bs-ride="carousel">
-            <div class="carousel-image-container">
-                <img src="https://images.unsplash.com/photo-1603380353725-f8a4d39cc41e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                    alt="">
-
-                <!-- Slide 1 -->
-                <div class="carousel-item active">
-                    <div class="carousel-container">
-                        <h2 class="animate__animated animate__fadeInDown">Videography</span></h2>
-                        <p class="animate__animated fanimate__adeInUp">Mengubah Momen Menjadi Karya Seni Bergerak.</p>
-                        <a href="#about"
-                            class="btn-get-started animate__animated animate__fadeInUp scrollto">Selengkapnya</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <svg class="hero-waves" xmlns="http://www.w3.org/2000/svg" style="position: absolute"
-            xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28 " preserveAspectRatio="none">
-            <defs>
-                <path id="wave-path" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z">
-            </defs>
-            <g class="wave1">
-                <use xlink:href="#wave-path" x="50" y="3" style="opacity: 25%" fill="#fff">
-            </g>
-            <g class="wave2">
-                <use xlink:href="#wave-path" x="50" y="0" style="opacity: 15%" fill="#fff">
-            </g>
-            <g class="wave3">
-                <use xlink:href="#wave-path" x="50" y="9" fill="#fff">
-            </g>
-        </svg>
-    </section>
-    <!-- End Hero -->
+    @include('layouts.carousel-category', [
+        'title' => 'Videography',
+        'description' => 'Mengubah Momen Menjadi Karya Seni Bergerak',
+        'buttonUrl' => '#about',
+        'buttonText' => 'Selengkapnya',
+    ])
 @endsection
 
 @section('main')
@@ -85,7 +55,7 @@
                 <div class="row justify-content-center">
                     @forelse ($categories as $category)
                         <div class="col-lg-4 col-md-6 mt-md-0 mt-5">
-                            <div class="icon-box rounded-3 text-center d-flex flex-column justify-content-between gap-3"
+                            <div class="icon-box rounded-3 d-flex flex-column justify-content-between gap-3 text-center"
                                 data-aos="zoom-in-left" data-aos-delay="100">
                                 <div class="">
                                     @if ($category->image === 'placeholder.jpg')
