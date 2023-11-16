f5198e4b955ce8f2a2c348afb9dbecaf2685de94
pradipta <mpradipta.arya02@gmail.com>
2023-11-16 07:54:05 +0700

    added layout for navbar dropdown

--- resources/views/layouts/category-header.blade.php
+++ resources/views/layouts/category-header.blade.php
@@ -10,29 +10,7 @@
                 <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
                 <li><a class="nav-link scrollto" href="#about">About</a></li>
                 <li><a class="nav-link scrollto" href="#services">Services</a></li>
-                @auth
-                    <li class="dropdown"><a href="#"><span>Welcome, {{ auth()->user()->name }}</span> <i
-                                class="bi bi-chevron-down"></i></a>
-                        <ul>
-                            @role('clent')
-                                <li><a href="{{ route('user.profile.index') }}">{{ auth()->user()->email }}</a></li>
-                                <li><a href="{{ route('user.order.list') }}">List Order</a></li>
-                            @else
-                                <li><a href="{{ route('dashboard') }}">{{ auth()->user()->email }}</a></li>
-                            @endrole
-                            <li>
-                                <form action="{{ route('logout') }}" method="post" class="d-inline">
-                                    @csrf
-                                    <a href="#"
-                                        onclick="return confirm('Yakin ? ') ? this.parentElement.submit() : null">Log
-                                        Out</a>
-                                </form>
-                            </li>
-                        </ul>
-                    </li>
-                @else
-                    <li><a class="nav-link" href="{{ route('login.index') }}">Login</a></li>
-                @endauth
+                @include('layouts.dropdown')
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
         </nav>
