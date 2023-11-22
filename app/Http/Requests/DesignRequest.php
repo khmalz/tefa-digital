75ca74ea01924666ea44122c7242903864fa64bc
khmalz <akmalrma1710@gmail.com>
2023-11-22 08:12:39 +0700

    fix: phone number must be started with 08/628/+628

--- app/Http/Requests/DesignRequest.php
+++ app/Http/Requests/DesignRequest.php
@@ -24,7 +24,7 @@ class DesignRequest extends FormRequest
         return [
             'design_plan_id' => ['required'],
             'name_customer' => ['required', 'string', 'max:191'],
-            'number_customer' => ['required', 'numeric', 'regex:/^(\+62|62|0*)[2-9]{1}[0-9]{5,20}$/'],
+            'number_customer' => ['required', 'numeric', 'regex:/^(\+628|628|08*)[2-9]{1}[0-9]{5,20}$/'],
             'email_customer' => ['required', 'email'],
             'slogan' => ['nullable', 'string', 'max:191'],
             'color' => ['required', 'string', 'max:191'],
